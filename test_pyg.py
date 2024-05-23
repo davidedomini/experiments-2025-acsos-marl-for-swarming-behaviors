151f5006b0ad61ed302e4651050915c4ca8141c2
Filippo-Venturini <filippo.venturini8@studio.unibo.it>
2024-05-23 11:51:18 +0200

    add GNN example

---
+++ test_pyg.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+from torch_geometric.datasets import Planetoid
+
+# Carica il dataset Cora
+dataset = Planetoid(root='/tmp/Cora', name='Cora')
+
+class GCN(torch.nn.Module):
+    def __init__(self):
+        super(GCN, self).__init__()
+        self.conv1 = GCNConv(dataset.num_node_features, 16)
+        self.conv2 = GCNConv(16, dataset.num_classes)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
+
+model = GCN()
+data = dataset[0]
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+
+model.train()
+for epoch in range(200):
+    optimizer.zero_grad()
+    out = model(data)
+    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+    loss.backward()
+    optimizer.step()
+
+# Valutazione del modello
+model.eval()
+_, pred = model(data).max(dim=1)
+correct = int(pred[data.test_mask].eq(data.y[data.test_mask]).sum().item())
+acc = correct / int(data.test_mask.sum())
+print(f'Accuracy: {acc:.4f}')
