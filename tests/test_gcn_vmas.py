32175f71754e668e1807852c84cf3f2a502dd4bd
Filippo-Venturini <filippo.venturini8@studio.unibo.it>
2024-07-01 11:39:01 +0200

    refactor simulator

--- tests/test_gcn_vmas.py
+++ tests/test_gcn_vmas.py
@@ -1,3 +1,4 @@
+from curses.ascii import SI
 import sys
 import os
 
@@ -24,6 +25,49 @@ model.load_state_dict(torch.load(models_dir + 'cohesion_collision.pth'))# Carica
 model.eval()
 print("Model loaded successfully!")
 
+class Simulator:
+    def __init__(self, env, model):
+        self.env = env
+        self.model = model
+
+    def run_simulation(self):
+        init_time = time.time()
+        total_reward = 0
+
+        observations = env.reset() 
+
+        for step in range(self.env.max_steps):
+            print(f"Step {step+1}")
+
+            graph_data = create_graph_from_observations(observations)
+
+            with torch.no_grad():
+                logits = self.model(graph_data)
+
+                print("Logits: ",logits)
+                
+                actions = torch.argmax(logits, dim=1)
+            
+            actions_dict = {f'agent{i}': torch.tensor([actions[i].item()]) for i in range(len(env.agents))}
+            
+            observations, rewards, _ , _ = env.step(actions_dict)
+
+            total_reward += sum(rewards.values())
+
+            frame = env.render(
+                mode="rgb_array",
+                agent_index_focus=None,
+                visualize_when_rgb=True,
+            )
+
+            total_time = time.time() - init_time
+            print(
+                f"It took: {total_time}s for {self.env.max_steps} steps of {env.num_envs} parallel environments with {total_reward} total reward, on device {env.device} "
+                f"for test_gcn_vmas scenario."
+            )
+
+
+
 env = make_env(
     CohesionScenario(),
     scenario_name="test_gcn_vmas",
@@ -34,51 +78,10 @@ env = make_env(
     wrapper=None,
     seed=None,
     n_agents=9,
+    max_steps= 100
 )
 
-render = True
-save_render = False
-frame_list = [] 
-init_time = time.time()
-n_steps = 100
-total_reward = 0
+if __name__ == "__main__":
+    simulator = Simulator(env, model)
+    simulator.run_simulation()
 
-observations = env.reset() 
-
-for step in range(n_steps):
-    print(f"Step {step+1}")
-
-    graph_data = create_graph_from_observations(observations)
-    #graph_data = torch.cat([observations[f'agent{i}'] for i in range(len(env.agents))], dim=0)
-
-    with torch.no_grad():
-        logits = model(graph_data)
-
-        #DEBUG: stampa i logits restituiti dal modello
-        print("Logits: ",logits)
-        
-        actions = torch.argmax(logits, dim=1)
-    
-    actions_dict = {f'agent{i}': torch.tensor([actions[i].item()]) for i in range(len(env.agents))}
-    
-    observations, rewards, done, _ = env.step(actions_dict)
-
-    total_reward += sum(rewards.values())
-
-    if render:
-        frame = env.render(
-            mode="rgb_array",
-            agent_index_focus=None,
-            visualize_when_rgb=True,
-        )
-        if save_render:
-            frame_list.append(frame)
-
-total_time = time.time() - init_time
-print(
-    f"It took: {total_time}s for {n_steps} steps of {env.num_envs} parallel environments with {total_reward} total reward, on device {env.device} "
-    f"for test_gcn_vmas scenario."
-)
-
-if render and save_render:
-    save_video("test_gcn_vmas", frame_list, fps=1 / env.scenario.world.dt)
