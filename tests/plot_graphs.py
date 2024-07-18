3c57bd569f89e19e4c218bc85829d1e67224dbb6
Filippo-Venturini <filippo.venturini8@studio.unibo.it>
2024-07-18 15:26:50 +0200

    plot charts

--- tests/plot_graphs.py
+++ tests/plot_graphs.py
@@ -1,28 +1,50 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file_path = 'hits_stats_4842.csv'
+file_path = 'stats/go_to_position/mean.csv'
 data = pd.read_csv(file_path)
 
-# Plot the trend of the reward
-plt.figure(figsize=(12, 6))
-plt.plot(data['Episode'], data['Hits'], label='Hits', color='b')
-plt.xlabel('Episode')
-plt.ylabel('Hits')
-plt.title('Hits Trend Over Episodes')
-plt.legend()
-plt.grid(True)
-plt.show()
-
-""" file_path = 'obstacle_avoidance_stats_4842.csv'
-data = pd.read_csv(file_path)
-
-# Plot the trend of the reward mean
 plt.figure(figsize=(12, 6))
 plt.plot(data['Episode'], data['Reward'], label='Reward', color='b')
 plt.xlabel('Episode')
 plt.ylabel('Reward')
-plt.title('Reward Mean Trend Over Episodes')
+plt.title('Obstacle Avoidance Rewards')
 plt.legend()
 plt.grid(True)
-plt.show() """
+plt.show()
+
+file_path = 'stats/flocking/mean.csv'
+data = pd.read_csv(file_path)
+
+plt.figure(figsize=(12, 6))
+plt.plot(data['Episode'], data['Reward'], label='Reward', color='b')
+plt.xlabel('Episode')
+plt.ylabel('Reward')
+plt.title('Obstacle Avoidance Rewards')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+file_path = 'stats/obstacle_avoidance/eval_mean.csv'
+data = pd.read_csv(file_path)
+
+plt.figure(figsize=(12, 6))
+plt.plot(data['Episode'], data['Reward'], label='Reward', color='b')
+plt.xlabel('Episode')
+plt.ylabel('Reward')
+plt.title('Obstacle Avoidance Rewards')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+file_path = 'stats/obstacle_avoidance/hits_mean.csv'
+data = pd.read_csv(file_path)
+
+plt.figure(figsize=(12, 6))
+plt.plot(data['Episode'], data['Hits'], label='Hits', color='b')
+plt.xlabel('Episode')
+plt.ylabel('Hits')
+plt.title('Obstacle Hits')
+plt.legend()
+plt.grid(True)
+plt.show()
